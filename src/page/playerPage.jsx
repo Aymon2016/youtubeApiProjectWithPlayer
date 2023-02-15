da3dd66858bd0265609dac7923f76b132989208c
Aymon2016 <mdaiman2016@gmail.com>
2023-02-15 11:34:33 +0600

    build commit

--- src/page/playerPage.jsx
+++ src/page/playerPage.jsx
@@ -1,17 +1,23 @@
 import { Col, Container, Row, Card } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import Player from '../component/playerPageComponent/player'
-import { useStoreState } from 'easy-peasy';
+import { useStoreState, useStoreActions } from 'easy-peasy';
 import VideoListItem from '../component/playerPageComponent/videoListItem';
+import { useEffect } from 'react';
 
 
 const PlayerPage = () => {
-
     const { playlistId } = useParams()
     const playlistArray = useStoreState((state) => Object.values(state.playlist.data))
     const playlistItem = playlistArray.filter((item) => item.playlistId == playlistId)
 
+    if (playlistItem.length == 0) {
+        const action = useStoreActions((action) => action.playlist)
+        action.getPlaylist(playlistId)
+    }
 
+    if (playlistItem.length == 0) return
+    console.log(playlistItem)
 
     return (
         <Container style={{ marginTop: '20px' }}>
